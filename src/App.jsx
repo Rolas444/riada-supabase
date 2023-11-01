6063a753bc35aa3e28f90023dd5d93c3fe1c723a
DESKTOP-PR8MKLN\hrodland <7200ws@gmail.com>
2023-11-01 17:07:15 -0500

    protección de rutas

--- src/App.jsx
+++ src/App.jsx
@@ -16,62 +16,34 @@ import IndexMinistry from './pages/ministries/IndexMinistry'
 import IndexUser from './pages/users/index-users'
 import CreateUser from './pages/users/create-user'
 import Users from './pages/users/users'
-import {ImSpinner9} from 'react-icons/im'
+
 
 function App() {
-  const navigate = useNavigate();
-  const [loading, setLoading] = useState(false);
-  const setCurrentUser = useAppStore((state) => state.setCurrentUser);
-
-  useEffect(() => {
-    setLoading(true);
-    supabase.auth.onAuthStateChange((event, session) => {
-      if (!session) {
-        setCurrentUser(null);
-        navigate("/login")
-      } else {
-        setCurrentUser(session.user)
-        setLoading(false);
-      }
-
-    });
-  }, [])
 
   return (
     <>
-      {
-        loading ?
-          (
-            <>
-              <div className='w-full h-screen flex justify-center items-center'>
-                  <ImSpinner9 className="animate-spin w-8 h-8" />
-              </div>
-            </>
-          ) : (
-            <>
-              <Routes>
-                <Route path='/login' element={<Login />} />
 
-                <Route path='/' element={<AdminLayout />}>
-                  <Route index element={<Home />} />
-                  <Route path='settings' element={<Settings />} />
-                  <Route path='admin/users' element={<Users />}>
-                    <Route index element={<IndexUser />} />
-                    <Route path='create' element={<CreateUser />} />
-                  </Route>
-                  <Route path='ministries' element={<Ministries />} >
-                    <Route index element={<IndexMinistry />} />
-                    <Route path='create' element={<CreateMinistry />} />
-                  </Route>
+      <Routes>
+        <Route path='/login' element={<Login />} />
 
-                  <Route path='persons' element={<Persons />} />
-                </Route>
-                <Route path='*' element={<NotFound />} />
-              </Routes >
-            </>
-          )
-      }
+        <Route path='/' element={<AdminLayout  />}>
+          <Route index element={<Home />} />
+          <Route path='settings' element={<Settings />} />
+          <Route path='admin/users' element={<Users />}>
+            <Route index element={<IndexUser />} />
+            <Route path='create' element={<CreateUser />} />
+          </Route>
+          <Route path='ministries' element={<Ministries />} >
+            <Route index element={<IndexMinistry />} />
+            <Route path='create' element={<CreateMinistry />} />
+          </Route>
+
+          <Route path='persons' element={<Persons />} />
+        </Route>
+        <Route path='*' element={<NotFound />} />
+      </Routes >
     </>
+
   )
 }
 
--- src/components/admin-layout.jsx
+++ src/components/admin-layout.jsx
@@ -1,22 +1,29 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Sidebar from "./Sidebar";
 import Navbar from "./Navbar";
 import { useEffect, useState } from "react";
 import { useAppStore } from "../zustand/AppStore";
 import { Outlet } from "react-router-dom";
 import Header from "./header";
+import InitAuth from "../tools/authentication";
+import { ImSpinner2 } from 'react-icons/im'
 
 const AdminLayout = ({ children }) => {
     // const [toggle, setToggle] = useState(false);
+    const navigate = useNavigate();
+    const [loading, setLoading] = useState(true)
     const toggle = useAppStore((state) => state.toggle);
     const smScreen = useAppStore((state) => state.smScreen);
     const setToggle = useAppStore((state) => state.setToggle);
     const setSmScreen = useAppStore((state) => state.setSmScreen);
+    const currentUser = useAppStore((state) => state.currentUser)
+    const setCurrentUser = useAppStore((state) => state.setCurrentUser);
 
 
     const handleSize = () => {
         // alert("me voy a ejecutar")
         // console.log(window.innerWidth)
+
         if (window.innerWidth > 768) {
 
             setToggle(false);
@@ -33,22 +40,52 @@ const AdminLayout = ({ children }) => {
         }
     }
 
+    const getSession = async () => {
+        setLoading(true)
+        const data = await InitAuth()
+        console.log(data)
+        if (data.session !== null) {
+            await setCurrentUser(data.user)
+            setLoading(false)
+        }
+        else {
+            setCurrentUser(null)
+            navigate('/login')
+        }
+
+    }
+
+
+
     useEffect(() => {
+
+        getSession()
         handleSize();
     }, [])
 
     return (
+
         <>
-            <div className="min-h-screen grid grid-cols-1 xl:grid-cols-6">
-                <Sidebar />
-                <div className="xl:col-span-5">
-                    <Header />
-                    <div className="h-[90vh] overflow-y-scroll p-8">
-                    <Outlet />
+            {
+                loading ? (
+                    <div className="w-full h-screen flex justify-center items-center">
+                        <ImSpinner2 className="w-8 h-8 animate-spin text-blue-600" />
                     </div>
-                    
-                </div>
-            </div>
+                ) :
+                    (
+                        <div className="min-h-screen grid grid-cols-1 xl:grid-cols-6">
+                            <Sidebar />
+                            <div className="xl:col-span-5">
+                                <Header />
+                                <div className="h-[90vh] overflow-y-scroll p-8">
+                                    <Outlet />
+                                </div>
+
+                            </div>
+                        </div>
+                    )
+
+            }
         </>
     )
 }
